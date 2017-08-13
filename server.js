9587fc40590833a2a0a2e0dd48e13c0f2551d48c
Esther Dama Kahindi <esther.kahindi@andela.com>
2017-08-13 06:56:20 +0300

    Add eslints and Add basic rooutes to use on postman

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Initializing the express app
+const app = express();
 
-const port = 3000
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(port, function () {
-    console.info('==> 🌎 Listening on port %s. Open up http://127.0.0.1:%s/ ', port, port);
-})
+// Set up the port
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// get an instance of the express Router
+const router = express.Router();
+
+// test route to make sure everything is working
+
+router.get('/', (req, res) => {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port, () => {
+  console.info('==> 🌎 Listening on port %s. Open up http://127.0.0.1:%s/ ', port, port);
+  console.info('==> 🌎 API port %s. Open up http://127.0.0.1:%s/api ', port, port);
+});
+
